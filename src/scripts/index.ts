0b53a9195650ddff3e738ccad1b1d5eea59fde46
tinzina <mr9ball@aol.com>
2023-01-19 08:54:50 -0600

    Initiate preview board using a FenRecord

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,26 @@
 import { runAllTests } from "./unit-test"
 import { game as g } from "./test-data/448600279"
-import { initPreview } from "./preview-test"
+import { board2d, squares } from "./preview-test"
+import { FenRecord } from "./fen-record"
 
-initPreview()
+const record = new FenRecord()
+const board = record.board
+
+function initPreview2d() {
+  for (let r = 0, n = 0; r < 8; r++) {
+    for (let c = 0; c < 8; c++, n++) {
+      board2d[r][c].dataset.role = board[r][c]
+    }
+  }
+}
+
+initPreview1d()
+
+function initPreview1d() {
+  const list = board.flat()
+  for (let i = 0; i < list.length; i++) {
+    squares[i].dataset.role = list[i]
+  }
+}
+
 console.log(Object.entries(g))
